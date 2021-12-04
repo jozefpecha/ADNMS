ca308857a5f084ffa7692cfdfac16f8e74d601a6
Jozef Pecha <70802421+jozefpecha@users.noreply.github.com>
2021-12-04 17:56:03 +0100

    Adapted for docking in python with Vina
    Outputs new sdf with sequential non-repeating names
    Creating pdb files of ligands made optional with -p argument

--- model_evaluation.py
+++ model_evaluation.py
@@ -16,9 +16,15 @@ def load_mols(in_file):
 	mols = [mol for mol in suppl]
 	smiles, matched_ids, matched_ids_count = [], [], []
 	for mol in mols:
-		smiles.append(Chem.MolToSmiles(mol, isomericSmiles=True, canonical=True))
+		new_name = f"F{(n+1):06d}_{mol.GetProp('_Name')}"
+		name = mol.SetProp("_Name", new_name)
+		smiles.append((Chem.MolToSmiles(mol, isomericSmiles=True, canonical=True), new_name))
 		matched_ids.append(mol.GetProp("matched_ids"))
 		matched_ids_count.append(mol.GetProp("matched_ids_count"))
+	
+	with Chem.SDWriter(os.path.join(out_file, "new_out.sdf")) as w:
+		for m in mols:
+			w.write(m)
 
 	return mols, smiles, matched_ids, matched_ids_count
 
@@ -33,32 +39,15 @@ def get_fp(mols):
 
 	return mfp_2
 
-
-
-def names(mols):
-	"""
-
-	param mols: list of RDKit Mols
-	return: sequential names of generated mols
-	"""
-	id_names = []
-	mol_names = [mol.GetProp("_Name") for mol in mols]
-	for i in range(1, len(mols)+1):
-		id_names.append(f"F{i:06d}_{mol_names[i-1]}")
-
-	return id_names
-
-
-def get_pdbs(mols, mol_names):
+def get_pdbs(mols):
 	"""
 
 	param mols: list of RDKit Mols
 	param mol_names: list of names of molecules
 	return: pdb files of generated molecules
 	"""
-	for mol, name in list(zip(mols, mol_names)):
-		mol.SetProp('_Name', name)
-		file_name = f"docking_results/pdb_files/{name}.pdb"
+	for mol in mols:
+		file_name = f"docking_results/pdb_files/{mol.GetProp('_Name')}.pdb"
 		file = os.path.join(args.out, file_name)
 		Chem.MolToPDBFile(mol, file)
 	
@@ -70,39 +59,25 @@ if __name__ == '__main__':
 	parser.add_argument('-i', '--input', metavar='STRING', required=True, help='path to input sdf file')
 	parser.add_argument('-r', '--ref', metavar='STRING', required=True, help='path to reference ligand pdb file')
 	parser.add_argument('-o', '--out', metavar='STRING', required=True, help='path to output directory')
+	parser.add_argument('-p', '--pdb', action='store_true', default=False, help='creates pdb files of ligands if specified')
 	args = parser.parse_args()
-
-	pp_output = args.out
-	os.makedirs(pp_output, exist_ok=True)
-	os.makedirs(os.path.join(pp_output, "docking_results", "pdb_files"), exist_ok=True)
-	os.makedirs(os.path.join(pp_output, "docking_results", "pdbqt_files"), exist_ok=True)
-	os.makedirs(os.path.join(pp_output, "docking_results", "pdbqt_models"), exist_ok=True)
-	os.makedirs(os.path.join(pp_output, "docking_results", "docking_logs"), exist_ok=True)
-	os.makedirs(os.path.join(pp_output, "docking_results", "rsmd", "smiles_files"), exist_ok=True)
-	os.makedirs(os.path.join(pp_output, "docking_results", "rsmd", "rsmd_results"), exist_ok=True)
 
 	mols, smiles, matched_ids, matched_ids_count = load_mols(args.input)
 	ref = AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromPDBFile(args.ref), 2)
 	ligands = get_fp(mols)
 	sim_scores = calc_similarity(ref, ligands)
-	mol_names = names(mols)
-	zipped = list(zip(smiles, mol_names))
 
 	# write out a file with all smiles
 	with open(os.path.join(args.out, 'smiles.smi'), "w") as f1:
-		for n, i in enumerate(zipped):
-			f1.write(zipped[n][0] + "\t" + zipped[n][1] + "\n")
-
-	# write out smiles files for each molecule (needed for rmsd calculation)
-	for n, i in enumerate(zipped):
-		with open(os.path.join(args.out, f"docking_results/rsmd/smiles_files/{i[1]}.smi"), "w") as f:
-			f.write(i[0] + "\t" + i[1])
+		for n, i in enumerate(smiles):
+			f1.write(smiles[n][0] + "\t" + smiles[n][1] + "\n")
 
 	# write out a temporary results file with combined data
 	with open(os.path.join(args.out, 'tmp_res.csv'), "w") as f2:
 		f2.write("Name" + "\t" + "Smiles" + "\t" + "matched_ids_count" + "\t" + "matched_ids" + "\t" + "sim_score" + "\n")
 		for n in range(len(mols)):
-			f2.write(mol_names[n] + '\t' + smiles[n] + '\t' + str(matched_ids_count[n]) + '\t' + matched_ids[n] + '\t' + str(round(sim_scores[n], 4)) + '\n')
+			f2.write(smiles[n][1] + '\t' + smiles[n][0] + '\t' + str(matched_ids_count[n]) + '\t' + matched_ids[n] + '\t' + str(round(sim_scores[n], 4)) + '\n')
 
-	get_pdbs(mols, mol_names)
+	if args.pdb:
+		get_pdbs(mols, mol_names)
 
