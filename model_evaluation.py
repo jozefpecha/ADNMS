0204faadf966126a6d9dc770b66c360dc85f75ea
jozefpecha <jozef.pecha@email.cz>
2022-01-04 23:56:59 +0100

    Script now can accept multiple reference smiles and returns corresponding similarity scores as separate columns in the ouput file

--- model_evaluation.py
+++ model_evaluation.py
@@ -14,11 +14,12 @@ def load_mols(in_file, out_file):
 	"""
 	suppl = Chem.SDMolSupplier(str(in_file))
 	mols = [mol for mol in suppl]
-	smiles, matched_ids, matched_ids_count = [], [], []
+	names, smiles, matched_ids, matched_ids_count = [], [], [], []
 	for n, mol in enumerate(mols):
 		new_name = f"F{(n+1):06d}_{mol.GetProp('_Name')}"
+		names.append(new_name)
 		mol.SetProp("_Name", new_name)
-		smiles.append((Chem.MolToSmiles(mol, isomericSmiles=True, canonical=True), new_name))		
+		smiles.append(Chem.MolToSmiles(mol, isomericSmiles=True, canonical=True))		
 		matched_ids.append(mol.GetProp("matched_ids"))
 		matched_ids_count.append(mol.GetProp("matched_ids_count"))
 	
@@ -26,7 +27,27 @@ def load_mols(in_file, out_file):
 		for m in mols:
 			w.write(m)
 
-	return mols, smiles, matched_ids, matched_ids_count
+	return mols, names, smiles, matched_ids, matched_ids_count
+
+def load_refs(ref_file):
+	"""
+
+	param ref_file: smi file containing smiles of reference molecules
+	return: list of reference molecules
+	"""
+	ref_mols = []
+	ref_mol_names = []
+	with open(ref_file, "r") as f:
+		for line in f:
+			ref_mol_name = line.strip().split('\t')[1]
+			ref_mol = Chem.MolFromSmiles(line.strip().split('\t')[0])
+			ref_mol.SetProp('_Name', ref_mol_name)
+
+			ref_mols.append(ref_mol)
+			ref_mol_names.append(ref_mol_name)
+			
+
+	return ref_mols, ref_mol_names
 
 
 def get_fp(mols):
@@ -58,26 +79,44 @@ def get_pdbs(mols):
 def main():
 	parser = argparse.ArgumentParser(description='Extract fragment smiles')
 	parser.add_argument('-i', '--input', metavar='STRING', required=True, help='path to input sdf file')
-	parser.add_argument('-r', '--ref', metavar='STRING', required=True, help='path to reference ligand pdb file')
+	parser.add_argument('-r', '--ref', metavar='FILE', required=True, help='path to file with reference molecules smiles and their tab separated names') # input as a file with reference smiles
 	parser.add_argument('-o', '--out', metavar='STRING', required=True, help='path to output directory')
 	parser.add_argument('-p', '--pdb', action='store_true', default=False, help='creates pdb files of ligands if specified')
 	args = parser.parse_args()
 
-	mols, smiles, matched_ids, matched_ids_count = load_mols(args.input, args.out)
-	ref = AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromPDBFile(args.ref), 2)
+	mols, names, smiles, matched_ids, matched_ids_count = load_mols(args.input, args.out)
+
+	ref_mols, ref_names = load_refs(args.ref)
+
+	ref_fps = get_fp(ref_mols)
 	ligands = get_fp(mols)
-	sim_scores = calc_similarity(ref, ligands)
+
+	sim_scores = {}
+	sim_scores['Name'] = names
+	sim_scores['smiles'] = smiles
+	sim_scores['matched_ids_count'] = matched_ids_count
+	sim_scores['matched_ids'] = matched_ids
+
+	for ref_fp in list(zip(ref_names, ref_fps)):
+		if not ref_fp[1]:
+			return None
+		sim_scores[ref_fp[0]] = calc_similarity(ref_fp[1], ligands)
+
+	df_results = pd.DataFrame.from_dict(sim_scores)
+	df.to_csv(os.path.join(args.out, 'tmp_res.csv'), index=False, sep='\t', na_rep='NaN')
+		
+
 
 	# write out a file with all smiles
 	with open(os.path.join(args.out, 'smiles.smi'), "w") as f1:
-		for n, i in enumerate(smiles):
-			f1.write(smiles[n][0] + "\t" + smiles[n][1] + "\n")
+		for i in list(zip(smiles, names)):
+			f1.write(i[0] + "\t" + i[1] + "\n")
 
-	# write out a temporary results file with combined data
-	with open(os.path.join(args.out, 'tmp_res.csv'), "w") as f2:
-		f2.write("Name" + "\t" + "Smiles" + "\t" + "matched_ids_count" + "\t" + "matched_ids" + "\t" + "sim_score" + "\n")
-		for n in range(len(mols)):
-			f2.write(smiles[n][1] + '\t' + smiles[n][0] + '\t' + str(matched_ids_count[n]) + '\t' + matched_ids[n] + '\t' + str(round(sim_scores[n], 4)) + '\n')
+	# write out a temporary results file with combined data --- 
+	#with open(os.path.join(args.out, 'tmp_res.csv'), "w") as f2:
+	#	f2.write("Name" + "\t" + "Smiles" + "\t" + "matched_ids_count" + "\t" + "matched_ids" + "\t" + "sim_score" + "\n")
+	#	for n in range(len(mols)):
+	#		f2.write(smiles[n][1] + '\t' + smiles[n][0] + '\t' + str(matched_ids_count[n]) + '\t' + matched_ids[n] + '\t' + str(round(sim_scores[n], 4)) + '\n')
 	
 	if args.pdb:
 		get_pdbs(mols, mol_names)
