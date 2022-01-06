54891f6bfe245aa46dd580c797532497ebb11c96
jozefpecha <jozef.pecha@email.cz>
2022-01-06 16:08:50 +0100

    Replaced spaces with tabs

--- model_evaluation_2.py
+++ model_evaluation_2.py
@@ -8,13 +8,13 @@ import seaborn as sns
 def extract_docking_scores(db_file):
 	conn = sqlite3.connect(db_file)
 	cur = conn.cursor()
-    
+	
 	query = cur.execute("SELECT id, docking_score FROM mols")
 	cols = ['Name', 'docking_score']
 	record = pd.DataFrame.from_records(data = query.fetchall(), columns=cols)
-    
+	
 	cur.close()
-    
+	
 	return record
 
 
@@ -29,11 +29,11 @@ def load_data(logs_db, rmsd_file, physchem_file, sim_scores_file, sa_scores_file
 	param output_dir: outpur directory for combined results
 	return: csv file containing combined results
 	"""
-	physchem = pd.read_csv(physchem_file, sep="\t", index_col=False)
-	sim_scores = pd.read_csv(sim_scores_file, sep="\t", index_col=False)
-	sa_scores = pd.read_csv(sa_scores_file, sep="\t", index_col=False, names = ['Name', 'SA_score'])
-	rmsd_scores = pd.read_csv(rmsd_file, sep="\t", index_col=False, names = ['Name', 'rmsd_score'], usecols=[0, 2])
-    	
+	physchem = pd.read_csv(physchem_file, sep="	", index_col=False)
+	sim_scores = pd.read_csv(sim_scores_file, sep="	", index_col=False)
+	sa_scores = pd.read_csv(sa_scores_file, sep="	", index_col=False, names = ['Name', 'SA_score'])
+	rmsd_scores = pd.read_csv(rmsd_file, sep="	", index_col=False, names = ['Name', 'rmsd_score'], usecols=[0, 2])
+		
 	dock_scores = extract_docking_scores(logs_db)
 
 	df1 = physchem.merge(sim_scores, how='outer', on='Name')
@@ -42,10 +42,10 @@ def load_data(logs_db, rmsd_file, physchem_file, sim_scores_file, sa_scores_file
 	df4 = df3.merge(dock_scores, how='outer', on='Name')
 
 	df4 = df4[['Name', 'Smiles', 'sim_score', 'docking_score', 'rmsd_score', 'SA_score', 'matched_ids', 'MW', 'MR', 'HBA', 'HBD',
-        'complexity', 'NumRings', 'RTB', 'TPSA', 'logP', 'Csp3', 'fmf', 'QED', 'HAC', 'NumRingsFused', 'N_unique_hba_hbd_atoms',
-        'max_ring_size', 'ChiralCenters']]
+		'complexity', 'NumRings', 'RTB', 'TPSA', 'logP', 'Csp3', 'fmf', 'QED', 'HAC', 'NumRingsFused', 'N_unique_hba_hbd_atoms',
+		'max_ring_size', 'ChiralCenters']]
 
-	df4.to_csv(os.path.join(output_dir, 'results.csv'), index=False, sep="\t")
+	df4.to_csv(os.path.join(output_dir, 'results.csv'), index=False, sep="	")
 
 	return df4
 
@@ -59,38 +59,38 @@ def plot_docking_scores(crem_mols_data, pp_output, comaprison_data_dir = None):
 	"""
 
 	def get_dockscore(pp_dockscores):
-    """
-    extracts the best docking score
-    param pp_dockscores: path to folder. The script expect that there are folders with docking results, `log` folder
-    return: list of tuples where there is molecule name on the first position and docking score on the second one
-    """
-    df_dock = []
-    for ll in os.listdir(pp_dockscores):
-        with open(os.path.join(pp_dockscores, ll)) as f:
-            ds = f.readlines()[26]
-            df_dock.append((os.path.splitext(ll)[0], float(ds.split()[1])))
-    return df_dock
+	"""
+	extracts the best docking score
+	param pp_dockscores: path to folder. The script expect that there are folders with docking results, `log` folder
+	return: list of tuples where there is molecule name on the first position and docking score on the second one
+	"""
+	df_dock = []
+	for ll in os.listdir(pp_dockscores):
+		with open(os.path.join(pp_dockscores, ll)) as f:
+			ds = f.readlines()[26]
+			df_dock.append((os.path.splitext(ll)[0], float(ds.split()[1])))
+	return df_dock
 
-    def plotting(pp_dock_res, pp_output):
-    """
+	def plotting(pp_dock_res, pp_output):
+	"""
 	plots docking scores if comparison data directory if specified
 	param pp_dock_res: directory with comparison molecules' docking scores in log files
 	param pp_out: path to output directory
-    """
-    df = pd.DataFrame(columns=['name', 'dockscore', 'status'])
-    for ll in os.listdir(pp_dock_res):
-        dft = pd.DataFrame(get_dockscore(os.path.join(pp_dock_res, ll, 'log')), columns=['name', 'dockscore'])
-        dft['status'] = [ll] * dft.shape[0]
-        df = df.append(dft, sort=False)
+	"""
+	df = pd.DataFrame(columns=['name', 'dockscore', 'status'])
+	for ll in os.listdir(pp_dock_res):
+		dft = pd.DataFrame(get_dockscore(os.path.join(pp_dock_res, ll, 'log')), columns=['name', 'dockscore'])
+		dft['status'] = [ll] * dft.shape[0]
+		df = df.append(dft, sort=False)
 
-    sns.histplot(df, x="dockscore", hue="status", element="step")
-    plt.title("Distribution of docking score for active, inactive and generated molecules")
-    plt.xlabel("Docking score")
-    plt.savefig(os.path.join(pp_output, 'dock_scores_histogram.svg'))
+	sns.histplot(df, x="dockscore", hue="status", element="step")
+	plt.title("Distribution of docking score for active, inactive and generated molecules")
+	plt.xlabel("Docking score")
+	plt.savefig(os.path.join(pp_output, 'dock_scores_histogram.svg'))
 
-    return None
+	return None
 
-    # if no comparison data, plots a simple histogram of dock scores of generated molecules
+	# if no comparison data, plots a simple histogram of dock scores of generated molecules
 	if comaprison_data_dir = None:
 		sns.histplot(data=crem_mols_data['docking_score'], kde=True, element='step')
 		plt.title('Distribution of docking score for generated molecules')
